d48b532c88feedfe417fbbf50393546b38b5e719
Nikita Karetnikov <nikita@karetnikov.org>
2015-08-31 07:30:26 +0300

    Add a simple configuration.nix file

---
+++ configuration.nix
@@ -0,0 +1,39 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.gummiboot.enable = true;
+  boot.loader.gummiboot.timeout = 5;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_3_18;
+  boot.initrd.kernelModules = [ "fbcon" ];
+  boot.initrd.luks.devices = [
+    { name = "main"; device = "/dev/sda3"; preLVM = true; }
+  ];
+
+  fileSystems = [ {
+    mountPoint = "/";
+    device = "/dev/main/main";
+  } {
+    mountPoint = "/home";
+    device = "/dev/main/home";
+  } {
+    mountPoint = "/boot";
+    device = "/dev/sda2";
+  }
+  ];
+
+  networking.hostName = "nixos";
+
+  # The NixOS release to be compatible with for stateful data such as databases.
+  system.stateVersion = "15.08";
+}
